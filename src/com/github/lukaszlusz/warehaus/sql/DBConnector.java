c7cb3784f3bb269a5c23a8a4d1954f515e6edbcb
Łukasz Łuszczyński <lukasz1917@gmail.com>
2016-11-21 00:37:21 +0100

    DBConnector changed from interface to abstract class

--- src/com/github/lukaszlusz/warehaus/sql/DBConnector.java
+++ src/com/github/lukaszlusz/warehaus/sql/DBConnector.java
@@ -2,7 +2,41 @@ package com.github.lukaszlusz.warehaus.sql;
 
 import java.sql.*;
 
-public interface DBConnector {
-    Connection getConnection() throws SQLException;
-    void closeConnection() throws SQLException;
+public abstract class DBConnector {
+    protected final String jdbcDriver;
+    protected String address;
+    protected String dbName;
+    protected String port;
+    protected String user;
+    protected String password;
+    protected Connection connection;
+
+    public DBConnector(String address, String dbName, String port, String user, String password, String jdbcDriver) {
+        this.address = address;
+        this.dbName = dbName;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+        this.jdbcDriver = jdbcDriver;
+    }
+
+    protected abstract String createURL();
+
+    protected void connect() throws SQLException{
+        try {
+            Class.forName(jdbcDriver);
+            connection = DriverManager.getConnection(createURL(),user,password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Connection getConnection() throws SQLException {
+        if (connection == null) connect();
+        return connection;
+    }
+    
+    public void closeConnection() throws SQLException {
+        if (connection != null) connection.close();
+    }
 }
--- src/com/github/lukaszlusz/warehaus/sql/DBConnectorMySQL.java
+++ src/com/github/lukaszlusz/warehaus/sql/DBConnectorMySQL.java
@@ -1,49 +1,19 @@
 package com.github.lukaszlusz.warehaus.sql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
-public class DBConnectorMySQL implements DBConnector {
-    private final String jdbcDriver = "com.mysql.jdbc.Driver";
-    private String address;
-    private String dbName;
-    private String port;
-    private String user;
-    private String password;
-    protected Connection connection;
+public class DBConnectorMySQL extends DBConnector {
 
     public DBConnectorMySQL(String address, String dbName, String port, String user, String password) {
-        this.address = address;
-        this.dbName = dbName;
-        this.port = port;
-        this.user = user;
-        this.password = password;
+        super(address, dbName, port, user, password, "com.mysql.jdbc.Driver");
     }
 
+    @Override
     protected String createURL() {
         String URL = "jdbc:mysql://";
         URL += address + ":" + port + "/" + dbName;
         return URL;
     }
 
-    protected void connect() throws SQLException{
-        try {
-            Class.forName(jdbcDriver);
-            connection = DriverManager.getConnection(createURL(),user,password);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 
-    @Override
-    public Connection getConnection() throws SQLException {
-        if (connection == null) connect();
-        return connection;
-    }
 
-    @Override
-    public void closeConnection() throws SQLException {
-        if (connection != null) connection.close();
-    }
 }
