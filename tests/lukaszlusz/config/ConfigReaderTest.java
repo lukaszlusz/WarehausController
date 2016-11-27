a973a6ede1b728cec3fdcd864bb8dc12e0a19845
Łukasz Łuszczyński <lukasz1917@gmail.com>
2016-11-27 14:13:51 +0100

    refactor

--- tests/lukaszlusz/config/ConfigReaderTest.java
+++ tests/lukaszlusz/config/ConfigReaderTest.java
@@ -9,12 +9,7 @@ public class ConfigReaderTest {
     public void correctXML() throws Exception {
         ConfigReader configReader = new ConfigReader();
         configReader.filepath = "./TestResourses/correct.xml";
-        DbInfo dbInfo = new DbInfo();
-        dbInfo.address = "127.0.0.1";
-        dbInfo.dbName = "test";
-        dbInfo.port = "3306";
-        dbInfo.user = "username";
-        dbInfo.password = "password";
+        DbInfo dbInfo = new DbInfo("127.0.0.1", "test", "3306", "username", "password");
         assertTrue(dbInfo.equals(configReader.getDbInfo()));
     }
 
